b57c68ab71508a0b12cfbf10bf5797e30a90455b
Michael Zhmaylo <92790655+MzHmO@users.noreply.github.com>
2024-03-22 23:01:30 +0500

    WTSQuerySessionInformation

---
+++ 0x9 - WTSQuerySessionInformation/main.cpp
@@ -0,0 +1,35 @@
+#include <windows.h>
+#include <locale>
+#include <iostream>
+
+void WhoIsWho();
+
+int main()
+{
+    setlocale(LC_ALL, "");
+    WhoIsWho();
+    return 0;
+}
+
+#include <WtsApi32.h>
+#pragma comment(lib, "Wtsapi32.lib")
+void WhoIsWho()
+{
+    DWORD sessionId = WTSGetActiveConsoleSessionId();
+
+    DWORD size = MAX_PATH;
+    LPTSTR pUserName = (LPTSTR)malloc(size);
+    if (WTSQuerySessionInformation(
+        WTS_CURRENT_SERVER_HANDLE, 
+        sessionId,
+        WTSUserName, 
+        &pUserName, 
+        &size)) { 
+
+        std::wcout << pUserName << std::endl;
+
+        WTSFreeMemory(pUserName);
+    }
+
+    return ;
+}
