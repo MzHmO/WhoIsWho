b16bb5cbe8f76d609790fe78c7938359466e062e
Michael Zhmailo <michael.zhmailo@yandex.ru>
2024-03-22 21:41:40 +0500

    Initial Commit

---
+++ 0x0 - GetUserNameW/main.cpp
@@ -0,0 +1,23 @@
+#include <windows.h>
+#include <locale>
+#include <iostream>
+
+void WhoIsWho();
+
+int main()
+{
+    setlocale(LC_ALL, "");
+    WhoIsWho();
+	return 0;
+}
+
+void WhoIsWho()
+{
+	DWORD size = 0;
+	LPWSTR username = NULL;
+	GetUserNameW(NULL, &size);
+	username = (LPWSTR)malloc(size + 1);
+	GetUserNameW(username, &size);
+    std::wcout << username << std::endl;
+    return;
+}
---
+++ 0x1 - GetEnvironmentVariableW/main.cpp
@@ -0,0 +1,23 @@
+#include <windows.h>
+#include <locale>
+#include <iostream>
+
+void WhoIsWho();
+
+int main()
+{
+    setlocale(LC_ALL, "");
+    WhoIsWho();
+	return 0;
+}
+
+void WhoIsWho()
+{
+	DWORD size = 256;
+	LPWSTR username = NULL;
+	username = (LPWSTR)malloc(size);
+	GetEnvironmentVariableW(L"USERNAME", username, size);
+	DWORD dw = GetLastError();
+	std::wcout << username << std::endl;
+    return;
+}
