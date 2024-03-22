67453389f27eac9b2c931cab4952f1c4e7dc74b6
Michael Zhmaylo <92790655+MzHmO@users.noreply.github.com>
2024-03-22 21:43:48 +0500

    GetTokenInformation

---
+++ 0x2 - GetTokenInformation/main.cpp
@@ -0,0 +1,57 @@
+#include <windows.h>
+#include <locale>
+#include <iostream>
+
+void WhoIsWho();
+
+int main()
+{
+	setlocale(LC_ALL, "");
+	WhoIsWho();
+	return 0;
+}
+
+void WhoIsWho()
+{
+	HANDLE hToken;
+	if (!OpenProcessToken(GetCurrentProcess(), TOKEN_READ, &hToken)) {
+		return;
+	}
+
+	DWORD bufferSize = 0;
+	if (!GetTokenInformation(hToken, TokenUser, NULL, 0, &bufferSize) && GetLastError() != ERROR_INSUFFICIENT_BUFFER) {
+		CloseHandle(hToken);
+		return;
+	}
+
+	PTOKEN_USER pTokenUser = (PTOKEN_USER)malloc(bufferSize);
+	if (!pTokenUser) {
+		CloseHandle(hToken);
+		return;
+	}
+
+	if (!GetTokenInformation(hToken, TokenUser, pTokenUser, bufferSize, &bufferSize)) {
+		free(pTokenUser);
+		CloseHandle(hToken);
+		return;
+	}
+
+	WCHAR accountName[MAX_PATH];
+	WCHAR domainName[MAX_PATH];
+	DWORD accountNameSize = MAX_PATH;
+	DWORD domainNameSize = MAX_PATH;
+	SID_NAME_USE snu;
+
+	if (!LookupAccountSidW(NULL, pTokenUser->User.Sid, accountName, &accountNameSize, domainName, &domainNameSize, &snu)) {
+		free(pTokenUser);
+		CloseHandle(hToken);
+		return;
+	}
+
+	std::wcout << accountName << std::endl;
+
+	free(pTokenUser);
+	CloseHandle(hToken);
+
+	return;
+}
